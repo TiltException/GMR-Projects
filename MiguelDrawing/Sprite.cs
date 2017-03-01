321884e1b180a66cb9e0cdabde8e8dd2589917e4
Miguel Verdugo <mver94@gmail.com>
2017-02-28 17:40:23 -0800

    Implemented Sprite Base class

--- MiguelDrawing/Sprite.cs
+++ MiguelDrawing/Sprite.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MiguelDrawing
 {
-    class Sprite
+    public class Sprite
     {
+        private Rectangle HitBox
+        {
+            get
+            {
+                return new Rectangle(Position, size);
+            }
+        }
+
+        public Point Position { get; set; }
+        public Point Speed { get; set; }
+
+        #region alternativeImplementation
+        //public int X
+        //{
+        //    get
+        //    {
+        //        return Position.X;
+        //    }
+        //    set
+        //    {
+        //        Position = new Point(value, Position.Y);
+        //    }
+        //}
+
+        //public int Y
+        //{
+        //    get
+        //    {
+        //        return Position.Y;
+        //    }
+        //    set
+        //    {
+        //        Position = new Point(Position.X, value);
+        //    }
+        //}
+        #endregion alternativeImplementation
+
+        private Size size;
+
+        private Brush color;
+
+
+        public Sprite(int x = 0, int y = 0, int xSpeed = 0, int ySpeed = 0, int height = 1, int width = 1, Brush color = null)
+            : this(new Point(x, y), new Point(xSpeed, ySpeed), new Size(height, width), color)
+        {
+        }
+        public Sprite(Point position, Point speed, Size size, Brush color)
+        {
+            Position = position;
+            Speed = speed;
+            this.size = size;
+            this.color = color;
+        }
     }
 }
